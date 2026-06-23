699743f82f52e4e97be54649058bc848f8135724
kenan-pamuk <kenan.pamuk@kiena.com.tr>
2026-06-24 02:16:56 +0300

    Update page.tsx - Added Glowyn page name

--- app/glowyn/page.tsx
+++ app/glowyn/page.tsx
@@ -2,6 +2,11 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Glowyn",
+};
 
 const modules = [
   {
