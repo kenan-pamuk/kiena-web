2b21251e923086fa2b7af10289e069df26abffbd
kenan-pamuk <kenan.pamuk@kiena.com.tr>
2026-06-24 02:19:40 +0300

    Update page.tsx fixed error

--- app/glowyn/page.tsx
+++ app/glowyn/page.tsx
@@ -1,12 +1,13 @@
-"use client";
-
-import Image from "next/image";
-import Link from "next/link";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Glowyn",
 };
+
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
 
 const modules = [
   {
