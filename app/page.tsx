c9c2de01e3190f9e6ff8c98eb6bf12073fd11959
kenan-pamuk <kenan.pamuk@kiena.com.tr>
2026-06-24 13:38:21 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import Header from "@/components/Header";
+import Header from "@/components/header";
 
 const features = [
   ['▣', 'Yapay Zekâ Odaklı', 'Akıllı algoritmalarla veriye dayalı çözümler sunarız.'],
