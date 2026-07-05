7a99bdf55e3f9864e6e8cf1a9ee1330975e3e60f
kenan-pamuk <kenan.pamuk@kiena.com.tr>
2026-07-05 23:49:45 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
-import Footer from '@/components/footer';
+import Footer from '../components/footer';
 
 export const metadata: Metadata = {
   title: {
