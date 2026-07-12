ad11464d284bcb866c3ebfdf8b2efdd8ec6b74ca
kenan-pamuk <kenan.pamuk@kiena.com.tr>
2026-07-12 17:21:43 +0300

    Update page.tsx

--- app/cozumlerimiz/page.tsx
+++ app/cozumlerimiz/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import Header from "../../components/header";
 
 export const metadata: Metadata = {
-  title: "Çözümlerimiz | Kiena",
+  title: "Çözümlerimiz",
 };
 
 export default function Page() {
