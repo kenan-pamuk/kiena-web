1ea6fa181ffdcbe29bd568d174ba736901bcb7b0
kenan-pamuk <kenan.pamuk@kiena.com.tr>
2026-08-11 22:39:34 +0300

    Create AboutVision.tsx

---
+++ components/about/AboutVision.tsx
@@ -0,0 +1,42 @@
+export default function AboutVision() {
+  return (
+    <section className="about-vision" id="vizyonumuz">
+      <div className="about-vision__content">
+        <div className="about-vision__text">
+          <span className="about-vision__eyebrow">VİZYONUMUZ</span>
+
+          <h2>
+            Yapay zekâ destekli kişisel yaşam teknolojilerinde{" "}
+            <span>global</span> ölçekte tercih edilen bir marka olmak.
+          </h2>
+
+          <p>
+            Yenilikçi çözümlerimizle, insanların hayatlarını daha planlı,
+            sağlıklı, verimli ve keyifli hale getirerek geleceğin yaşam
+            standartlarını bugünden inşa ediyoruz.
+          </p>
+        </div>
+
+        <div className="about-vision__art" aria-hidden="true">
+          <div className="about-vision__sun" />
+
+          <div className="about-vision__city">
+            <span />
+            <span />
+            <span />
+            <span />
+            <span />
+            <span />
+            <span />
+            <span />
+            <span />
+          </div>
+
+          <div className="about-vision__road about-vision__road--1" />
+          <div className="about-vision__road about-vision__road--2" />
+          <div className="about-vision__road about-vision__road--3" />
+        </div>
+      </div>
+    </section>
+  );
+}
